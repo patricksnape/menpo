a4227c20fc1bdd1e9f52f195b3a030b55aa029f5
Patrick Snape <patricksnape@gmail.com>
2014-09-18 11:56:49 +0100

    Add Federica's features to the __init__

--- menpo/feature/__init__.py
+++ menpo/feature/__init__.py
@@ -1,3 +1,8 @@
 from .features import gradient, hog, lbp, es, igo, gbp, gh, no_op, gaussian_filter
 from .predefined import sparse_hog, double_igo
 from .base import ndfeature, imgfeature
+from .features import (approximate_normals, K_gaussian_curvature,
+                       C_curvature_index, depth, E_first_coeff, e_second_coeff,
+                       F_first_coeff, f_second_coeff, G_first_coeff,
+                       g_second_coeff, H_mean_curvature, k1_principal_curvature,
+                       k2_principal_curvature, S_shape_index)
