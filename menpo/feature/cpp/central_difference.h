d1fbc90ea1f341fbeaa5c31b8fbb43dfb3abee05
Patrick Snape <patricksnape@gmail.com>
2015-01-07 15:27:23 +0000

    Fix the types for the gradient
    Move back to memoryviews and use the new 'get buffer type'
    method from the cy_utils.pxd

--- menpo/feature/cpp/central_difference.h
+++ menpo/feature/cpp/central_difference.h
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-static inline long long SUB2IND(const long long j, const long long i, const long long k,
-                                const long long row_size, const long long col_size,
-                                const long long n_channels) {
+static inline size_t SUB2IND(const size_t j, const size_t i, const size_t k,
+                             const size_t row_size, const size_t col_size,
+                             const size_t n_channels) {
     //return ((i + col_size * j) * n_channels) + k;
     return (i + col_size * j) + (row_size * col_size * k);
 }
 
 template<typename T>
-void central_difference(const T* in, const long long rows,
-                        const long long cols, const long long n_channels,
+void central_difference(const T* in, const size_t rows, const size_t cols,
+                        const size_t n_channels,
                         T* out) {
-    const long long n_output_channels = n_channels * 2;
-    long long output_index = 0;
+    const size_t n_output_channels = n_channels * 2;
+    size_t output_index = 0;
 
     #pragma omp for
-    for (long long k = 0; k < n_channels; ++k) {
+    for (size_t k = 0; k < n_channels; ++k) {
         // row-derivative
-        for (long long i = 0; i < cols; ++i) {
-            for (long long j = 0; j < rows; ++j) {
+        for (size_t i = 0; i < cols; ++i) {
+            for (size_t j = 0; j < rows; ++j) {
                 if (j == 0) {
                     output_index = SUB2IND(0, i, k, rows, cols, n_output_channels);
                     out[output_index] = in[SUB2IND(1, i, k, rows, cols, n_channels)] - in[SUB2IND(0, i, k, rows, cols, n_channels)];
@@ -35,8 +35,8 @@ void central_difference(const T* in, const long long rows,
         }
 
         // column-derivative
-        for (long long j = 0; j < rows; ++j) {
-            for (long long i = 0; i < cols; ++i) {
+        for (size_t j = 0; j < rows; ++j) {
+            for (size_t i = 0; i < cols; ++i) {
                 if (i == 0) {
                     output_index = SUB2IND(j, 0, n_channels + k , rows, cols, n_output_channels);
                     out[output_index] = in[SUB2IND(j, 1, k, rows, cols, n_channels)] - in[SUB2IND(j, 0, k, rows, cols, n_channels)];
--- menpo/feature/gradient.pyx
+++ menpo/feature/gradient.pyx
@@ -3,33 +3,37 @@
 import numpy as np
 cimport numpy as np
 cimport cython
+from menpo.cy_utils cimport dtype_from_memoryview
 
 
-ctypedef fused DOUBLE_TYPES:
+ctypedef fused IMAGE_TYPES:
     float
     double
 
 
 cdef extern from "cpp/central_difference.h":
-    void central_difference[T](const T* input, const Py_ssize_t rows,
-                               const Py_ssize_t cols, const Py_ssize_t n_channels,
+    void central_difference[T](const T* input, const size_t rows,
+                               const size_t cols, const size_t n_channels,
                                T* output)
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef gradient_cython(np.ndarray[DOUBLE_TYPES, ndim=3] input):
+cpdef gradient_cython(IMAGE_TYPES[:, :, :] input):
 
-    cdef Py_ssize_t n_channels = input.shape[0]
-    cdef Py_ssize_t rows = input.shape[1]
-    cdef Py_ssize_t cols = input.shape[2]
+    cdef:
+        size_t n_channels_in = input.shape[0]
+        size_t rows = input.shape[1]
+        size_t cols = input.shape[2]
+        size_t n_channels_out = n_channels_in * 2
+
     # Maintain the dtype that was passed in (float or double)
-    dtype = input.dtype
-    cdef np.ndarray[DOUBLE_TYPES, ndim=3] output = np.zeros((n_channels * 2,
-                                                            rows, cols),
-                                                            dtype=dtype)
+    dtype = dtype_from_memoryview(input)
+    cdef IMAGE_TYPES[:, :, :] output = np.zeros((n_channels_out, rows, cols),
+                                                dtype=dtype)
 
-    central_difference(&input[0,0,0], rows, cols, n_channels,
-                       &output[0,0,0])
+    central_difference(&input[0, 0, 0], rows, cols, n_channels_in,
+                       &output[0, 0, 0])
 
-    return output
+    # As numpy array without copying
+    return np.asarray(output)
